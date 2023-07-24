63630b9e0baecf9185192f9c50209dd29271cbfc
Joanna Graczyk <joanna.graczyk1@gmail.com>
2023-07-24 17:47:23 +0200

    Add colors and constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+// Primary Background
+export const white = "#ffffff";
+export const primaryBackground = white;
+
+// Secondary Background
+export const orange = "#fabb77";
+export const secondaryBackground = orange;
+
+//  Primary Foreground
+export const darkGrey = "#454646";
+export const primaryForeground = darkGrey;
+
+// Secondary Foreground
+export const purple = "#806ad8";
+export const secondaryForeground = purple;
+
+// Accents
+export const lightPurple = "#eeefff";
+export const coral = "#f59a96";
+export const lightDoctorGreen = "#9ef3e2";
+export const darkDoctorGreen = "#6ce1c9";
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const MENU_HEIGHT = 70;
+export const MOBILE_SIDE_PADDING = 18;
+export const MOBILE_BREAKPOINT = 500;
